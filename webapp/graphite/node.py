e363c7b9b0a2d9a1cd212254e6977d062f72c3c7
clarsen <clarsen@llnw.com>
2013-06-04 10:38:57 -0400

    Add dictionary path splitter to node.py Add UID field to node.py

--- webapp/graphite/node.py
+++ webapp/graphite/node.py
@@ -1,13 +1,24 @@
 
 
 class Node(object):
-  __slots__ = ('name', 'path', 'local', 'is_leaf')
+  __slots__ = ('name', 'path', 'local', 'is_leaf', 'uid')
 
   def __init__(self, path):
-    self.path = path
-    self.name = path.split('.')[-1]
+    if (type(path) == dict):
+      keys = sorted(path.iterkeys())
+      for key in keys:
+        if (int(key) == 1):
+          self.path = path[key]
+        elif (int(key) > 0):
+          self.path = self.path + "|" + path[key]
+      self.name = path[keys[-1]]
+    else:
+      self.path = path
+      self.name = path.split('.')[-1]
+      
     self.local = True
     self.is_leaf = False
+    self.uid = None
 
   def __repr__(self):
     return '<%s[%x]: %s>' % (self.__class__.__name__, id(self), self.path)
@@ -25,6 +36,7 @@ class LeafNode(Node):
     self.reader = reader
     self.intervals = reader.get_intervals()
     self.is_leaf = True
+    self.uid = None
 
   def fetch(self, startTime, endTime):
     return self.reader.fetch(startTime, endTime)
